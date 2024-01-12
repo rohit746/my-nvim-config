f66f6e50db7fe2918a1b9b27c4856938c3492471
Rohit Jadhav <45796556+rohit746@users.noreply.github.com>
2024-01-12 19:12:25 +0530

    snippets using lua scippets.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>se", function()
+  require("scissors").editSnippet()
+end, { desc = "Edit snippet" })
+
+-- When used in visual mode prefills the selection as body.
+vim.keymap.set({ "n", "x" }, "<leader>sA", function()
+  require("scissors").addNewSnippet()
+end, { desc = "Create new snippet" })
